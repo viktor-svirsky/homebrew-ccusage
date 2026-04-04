ea0269888ae03d57ec553f9155cef37e6b8a555f
Viktor Svirskyi <viktor.svirskyi@icloud.com>
2026-04-04 01:49:37 +0000

    Update CCUsage to 1.20.0

--- Casks/ccusage.rb
+++ Casks/ccusage.rb
@@ -1,6 +1,6 @@
 cask "ccusage" do
-  version "1.19.0"
-  sha256 "9664da6db6c21ed57bd6dfa5a86c8143552320ce0924705f7947d2364ed6c53c"
+  version "1.20.0"
+  sha256 "02c2ad050087e625613ec46e31a3bc99f313213f47e9af22de49fff0097c0b20"
 
   url "https://github.com/viktor-svirsky/ccusage/releases/download/v#{version}/CCUsage.zip"
   name "CCUsage"
